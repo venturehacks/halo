24dac59c51268dab890739b59dd558e577c4c75a
Drew Lustro <drew@angel.co>
2019-02-18 21:42:56 -0800

    stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'declaration-empty-line-before': 'never',
+    'max-empty-lines': 1,
+    'no-invalid-double-slash-comments': null,
+    'number-leading-zero': 'always',
+    'rule-empty-line-before': [
+      'always',
+      {
+        ignore: ['inside-block'],
+      },
+    ],
+    'scss/at-rule-no-unknown': true,
+    'scss/double-slash-comment-inline': null,
+    'scss/double-slash-comment-whitespace-inside': 'always',
+    'scss/double-slash-comment-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+      },
+    ],
+    'selector-attribute-quotes': 'always',
+    'string-quotes': 'single',
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+    'selector-pseudo-element-colon-notation': 'single',
+  },
+};
