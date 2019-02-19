4a15485febf5dbb56877612c773c1ea52c5497d4
Drew Lustro <drew@angel.co>
2019-02-19 00:43:11 -0800

    <Paragraph />

--- src/components/elements/Paragraph/index.tsx
+++ src/components/elements/Paragraph/index.tsx
@@ -1,25 +1,28 @@
 import classNames from 'classnames';
 import * as React from 'react';
-import { SFC } from 'react';
 
 import * as styles from './styles.scss';
 
-export interface ParagraphProps {
-  children?: React.ReactNode;
+export interface Props {
+  preset: 'normal' | 'support' | 'micro' | 'error';
+  flow: boolean;
+  children: React.ReactNode;
   className?: string;
 }
 
-class Paragraph extends React.PureComponent<ParagraphProps> {
-  static defaultProps = {
-    variant: 'primary',
-    size: 'regular',
-    disabled: false,
-  };
+export default function Paragraph({ flow = false, preset = 'normal', children, className }: Props) {
+  const classes = classNames(
+    styles.component,
+    preset === 'support' && styles.presetSupport,
+    preset === 'micro' && styles.presetMicro,
+    preset === 'error' && styles.presetError,
+    flow && styles.flow,
+    className,
+  );
 
-  render(): React.ReactNode {
-    return '<Paragraph />';
-  }
+  return (
+    <p className={classes}>
+      {children}
+    </p>
+  );
 }
-
-export { Paragraph };
-export default Paragraph;
--- src/components/elements/Paragraph/styles.scss.d.ts
+++ src/components/elements/Paragraph/styles.scss.d.ts
@@ -1,1 +1,5 @@
 export const component: string;
+export const presetSupport: string;
+export const presetMicro: string;
+export const presetError: string;
+export const flow: string;
