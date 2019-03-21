4e6d100902deb1f00448bc90fab7a265e46dc844
Tiffany <tiffany@twu.io>
2019-03-21 09:45:42 -0400

    adjust PillTag font-weight

--- src/components/elements/PillTag/index.tsx
+++ src/components/elements/PillTag/index.tsx
@@ -6,11 +6,33 @@ import * as styles from './styles.scss';
 export interface PillTagProps {
   children?: React.ReactNode;
   className?: string;
+  href?: string;
+  onClick?: () => any;
 }
 
-function PillTag({ children, className }: PillTagProps) {
-  const classes = classNames(styles.component);
-  return <span className={classes}>{children}</span>;
+function PillTag({ children, className, href, onClick }: PillTagProps) {
+  const classes = classNames(styles.component, className);
+
+  if (href) {
+    return (
+      <a className={classes} href={href}>
+        {children}
+      </a>
+    );
+  }
+
+  const buttonProps = onClick
+    ? {
+        onClick,
+        role: 'button',
+      }
+    : {};
+
+  return (
+    <span className={classes} {...buttonProps}>
+      {children}
+    </span>
+  );
 }
 
 export { PillTag };
