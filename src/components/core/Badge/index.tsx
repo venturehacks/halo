a34a85a112cfe327e997953d346d60360a77d0fb
Drew Lustro <drew@angel.co>
2019-04-18 21:22:00 -0700

    :sparkles: <Badge />

---
+++ src/components/core/Badge/index.tsx
@@ -0,0 +1,46 @@
+import classNames from 'classnames';
+import * as React from 'react';
+import { Tooltip } from '~/components/core/Tooltip';
+
+import * as styles from './styles.scss';
+
+export type BadgeColor =
+  | 'blue'
+  | 'gray'
+  | 'green'
+  | 'orange'
+  | 'purple'
+  | 'red';
+
+export interface BadgeProps {
+  children?: React.ReactNode;
+  className?: string;
+  color?: BadgeColor;
+  shape?: 'square' | 'circle';
+  tooltip?: string;
+}
+
+function Badge({
+  children,
+  className,
+  color = 'gray',
+  shape = 'circle',
+  tooltip,
+}: BadgeProps) {
+  const component = (
+    <div
+      className={classNames(
+        styles.component,
+        styles[color],
+        styles[shape],
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+
+  return tooltip ? <Tooltip content={tooltip}>{component}</Tooltip> : component;
+}
+
+export { Badge };
