9b5b652c286cc0c7b784f5bd41ade5f56772fc1e
Drew Lustro <drew@angel.co>
2019-04-18 21:21:53 -0700

    :sparkles: <Avatar />

---
+++ src/components/core/Avatar/Avatar.tsx
@@ -0,0 +1,61 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Badge, BadgeColor } from '~/components/core/Badge';
+import { ForwardedRefProps, withForwardedRef } from '~/lib/withForwardedRef';
+
+import styles from './styles.scss';
+
+export type AvatarShape = 'circle' | 'square';
+
+export type AvatarSize = 'xxsmall' | 'xsmall' | 'small' | 'medium' | 'large';
+
+interface Props extends ForwardedRefProps {
+  badge?: string | boolean;
+  badgeColor?: BadgeColor;
+  className?: string;
+  imageUrl: string;
+  name: string;
+  shape?: AvatarShape;
+  size: AvatarSize;
+  tooltip?: string;
+}
+
+function AvatarRaw({
+  badge = false,
+  badgeColor,
+  className,
+  imageUrl,
+  name,
+  shape = 'circle',
+  size = 'medium',
+  tooltip,
+  forwardedRef,
+}: Props) {
+  const badgeOptions = {
+    color: badgeColor,
+    tooltip,
+  };
+
+  return (
+    <div
+      className={classNames(
+        styles.component,
+        className,
+        styles[shape],
+        styles[size],
+      )}
+      ref={forwardedRef}
+    >
+      <img
+        className={classNames(styles.avatar, styles[shape], styles[size])}
+        src={imageUrl}
+        alt={`Avatar for ${name}`}
+      />
+      {badge && <Badge {...badgeOptions}>{badge}</Badge>}
+    </div>
+  );
+}
+
+const Avatar = withForwardedRef(AvatarRaw);
+
+export { Avatar };
--- src/components/core/Avatar/styles.scss.d.ts
+++ src/components/core/Avatar/styles.scss.d.ts
@@ -1,5 +1,13 @@
 declare const styles: {
   readonly "component": string;
+  readonly "xxsmall": string;
+  readonly "xsmall": string;
+  readonly "small": string;
+  readonly "medium": string;
+  readonly "large": string;
+  readonly "avatar": string;
+  readonly "circle": string;
+  readonly "square": string;
 };
 export = styles;
 
