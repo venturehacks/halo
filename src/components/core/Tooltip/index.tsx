bdf807e1fb9c04d0e0031f7900bf55822f776dc1
Drew Lustro <drew@angel.co>
2019-04-29 16:42:17 -0700

    Synthetic default import on Avatar seems to break JSX autocomplete

--- src/components/core/Tooltip/index.tsx
+++ src/components/core/Tooltip/index.tsx
@@ -7,8 +7,6 @@ import styles from './styles.scss';
  * NOTE: If you want to use a component element as a child, ensure you forward
  * the ref to the DOM node:
  *
- * import React, { forwardRef } from 'react'
- *
  * const ThisWillWork = forwardRef((props, ref) => (
  *   <button ref={ref}>Text</button>
  * ));
