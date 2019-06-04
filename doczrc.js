aa52980ed655524f41b8c35947f00a48e0e0b150
Drew Lustro <drew@angel.co>
2019-06-03 18:14:37 -0700

    Another try at docs

--- doczrc.js
+++ doczrc.js
@@ -9,6 +9,7 @@ export default {
   notUseSpecifiers: true,
   src: path.join(__dirname, 'src'),
   filterComponents: files => files.filter(filepath => /components\/.*\/*\.(js|jsx|ts|tsx)$/.test(filepath)),
+  base: '/halo/',
   dest: '/docs',
   onCreateWebpackChain: (config) => {
     if (config.resolve.alias.set) {
