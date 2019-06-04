7c6f5f22b661454819da8e930fbc5b95203f0c23
Drew Lustro <drew@angel.co>
2019-06-03 18:04:38 -0700

    Prepare for docs deploy

--- doczrc.js
+++ doczrc.js
@@ -9,6 +9,7 @@ export default {
   notUseSpecifiers: true,
   src: path.join(__dirname, 'src'),
   filterComponents: files => files.filter(filepath => /components\/.*\/*\.(js|jsx|ts|tsx)$/.test(filepath)),
+  dest: '/docs',
   onCreateWebpackChain: (config) => {
     if (config.resolve.alias.set) {
       config.resolve.alias.set('~/components', path.join(__dirname, 'src', 'components'));
