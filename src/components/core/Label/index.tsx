e35427c98344761fda2c7dc6d6ae8a748bee47d6
Drew Lustro <drew@angel.co>
2019-07-23 12:26:20 -0700

    Import Tiff's enhanced Label from CoPro; add 2x more colors

--- src/components/core/Label/index.tsx
+++ src/components/core/Label/index.tsx
@@ -1,22 +1,42 @@
 import classNames from 'classnames';
 import * as React from 'react';
 
+import {
+  ForwardedRefProps,
+  withForwardedRef,
+} from '../../../lib/withForwardedRef';
+
 import styles from './styles.scss';
 
 interface LabelProps {
   className?: string;
   children: React.ReactNode;
-  color?: 'blue' | 'gray';
+  size: 'xs' | 'sm' | 'md';
+  color?: 'blue' | 'gray' | 'orange' | 'red' | 'purple' | 'green';
 }
 
-function Label({ children, className, color = 'blue' }: LabelProps) {
+function LabelRaw({
+  children,
+  className,
+  forwardedRef,
+  size = 'sm',
+  color = 'blue',
+}: LabelProps & ForwardedRefProps) {
   const classes = classNames(
     styles.component,
     className,
-    color && styles[color],
+    styles[color],
+    styles[size],
   );
 
-  return <span className={classes}>{children}</span>;
+  return (
+    <span className={classes} ref={forwardedRef}>
+      {children}
+    </span>
+  );
 }
 
+const Label = withForwardedRef<LabelProps>(LabelRaw);
+
+export default Label;
 export { Label };
