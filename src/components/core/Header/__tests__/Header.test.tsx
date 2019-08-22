01c9dec631ea6baaacdcfa4ca1a2235dd78cd014
Drew Lustro <drew@angel.co>
2019-08-22 13:51:32 -0700

    [Span] Don't propogate invalid props to HTML tag

--- src/components/core/Header/__tests__/Header.test.tsx
+++ src/components/core/Header/__tests__/Header.test.tsx
@@ -7,3 +7,12 @@ test('Smoke test', () => {
   const component = render(<Header h1>Play Tetris</Header>);
   expect(component.text()).toContain('Play Tetris');
 });
+
+test(`Invalid props don't propagate to HTML tag`, () => {
+  const component = render(
+    <Header h6 xxmuted uppercase>
+      Play Tetris
+    </Header>,
+  );
+  expect(component.text()).toContain('Play Tetris');
+});
