b3f073948bb714e52cb941949e393f88ed5ec7b4
Drew Lustro <drew@angel.co>
2019-09-17 15:54:32 -0700

    RawTextarea docs

--- src/components/form/RawTextarea/index.tsx
+++ src/components/form/RawTextarea/index.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 
 import styles from './styles.scss';
 
-interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+export interface RawTextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   className?: string;
 }
 
-export default function RawTextarea({ className, ...rest }: Props) {
+function RawTextarea({ className, ...rest }: RawTextareaProps) {
   return (
     <textarea className={classNames(styles.component, className)} {...rest} />
   );
 }
+
+export { RawTextarea };
