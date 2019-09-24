c3b0e85790ed59a66b844d8437d5fb228a55177e
Drew Lustro <drew@angel.co>
2019-09-23 17:35:02 -0700

    Force commit autogenerated 'double-quote' d.ts files

--- src/components/form/RawRadio/styles.scss.d.ts
+++ src/components/form/RawRadio/styles.scss.d.ts
@@ -1,11 +1,11 @@
 declare const styles: {
-  readonly 'component': string;
-  readonly 'hasError': string;
-  readonly 'warning': string;
-  readonly 'critical': string;
-  readonly 'label': string;
-  readonly 'asCheckbox': string;
-  readonly 'asRadio': string;
+  readonly "component": string;
+  readonly "hasError": string;
+  readonly "warning": string;
+  readonly "critical": string;
+  readonly "label": string;
+  readonly "asCheckbox": string;
+  readonly "asRadio": string;
 };
 export = styles;
 
