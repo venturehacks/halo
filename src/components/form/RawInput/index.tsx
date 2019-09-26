5a7dbe065624b4e53e28d39d12f4a4e25e83e8c0
Drew Lustro <drew@angel.co>
2019-09-26 16:38:28 -0700

    [RawInput] fix inner Box to use flexbox (instead of new default block)

--- src/components/form/RawInput/index.tsx
+++ src/components/form/RawInput/index.tsx
@@ -108,6 +108,7 @@ export default function RawInput({
     <div className={styles.wrapper}>
       {input}
       <Box
+        column
         valign="center"
         align="center"
         className={classNames(
