757453e2a649b9ec7e20cff8e3c82f0757e414f4
Drew Lustro <drew@angel.co>
2019-12-12 14:06:36 -0800

    incorporate follow-up @lllychen feedback

--- src/components/core/PillTag/index.tsx
+++ src/components/core/PillTag/index.tsx
@@ -7,11 +7,26 @@ export interface PillTagProps {
   children?: React.ReactNode;
   className?: string;
   href?: string;
+  /**
+   * Explicit hint that this pill tag is interactive
+   * @default false
+   */
+  interactive?: boolean;
   onClick?: EventFunctionT;
 }
 
-function PillTag({ children, className, href, onClick }: PillTagProps) {
-  const classes = classNames(styles.component, className);
+function PillTag({
+  children,
+  className,
+  href,
+  interactive,
+  onClick,
+}: PillTagProps) {
+  const classes = classNames(
+    styles.component,
+    className,
+    (href || interactive) && styles.anchor,
+  );
 
   if (href) {
     return (
