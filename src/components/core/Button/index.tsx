10723d60d7161c5e2bd09e3a0562b23e65b90c1b
Drew Lustro <drew@angel.co>
2019-12-13 00:49:53 -0800

    [ClearButton] :sparkles:
    + style tightening on existing sizes
    + better documentation

--- src/components/core/Button/index.tsx
+++ src/components/core/Button/index.tsx
@@ -16,8 +16,15 @@ export type ButtonVariant =
   | 'warning'
   | 'danger'
   | 'success'
-  | 'gray'
   | 'secondary-gray'
+  | 'clear'
+  /**
+   * @deprecated 0.9
+   */
+  | 'gray'
+  /**
+   *  @deprecated 0.9
+   */
   | 'alternate';
 
 export interface ButtonProps {
@@ -46,6 +53,8 @@ export interface ButtonProps {
    * Main control for button style
    */
   variant?: ButtonVariant;
+
+  width?: '100%';
 }
 
 function ButtonRaw({
@@ -61,6 +70,7 @@ function ButtonRaw({
   size = 'regular',
   type = 'button',
   variant = 'primary',
+  width,
   ...buttonElementProps
 }: ButtonProps &
   React.HTMLAttributes<HTMLButtonElement> &
@@ -73,6 +83,7 @@ function ButtonRaw({
     emphasis && styles.emphasis,
     icon && styles.icon,
     icon && !children && styles.iconOnly,
+    width === '100%' && styles.width100,
     className,
   ]);
 
--- src/components/core/Button/styles.scss.d.ts
+++ src/components/core/Button/styles.scss.d.ts
@@ -7,16 +7,18 @@ declare const styles: {
   readonly "success": string;
   readonly "gray": string;
   readonly "secondaryGray": string;
+  readonly "clear": string;
   readonly "alternate": string;
-  readonly "large": string;
+  readonly "regular": string;
   readonly "text": string;
   readonly "iconOnly": string;
-  readonly "regular": string;
+  readonly "large": string;
   readonly "small": string;
   readonly "xsmall": string;
   readonly "emphasis": string;
   readonly "inline": string;
   readonly "icon": string;
+  readonly "width100": string;
 };
 export = styles;
 
