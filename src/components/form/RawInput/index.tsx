0c98a2bd81f25e246ea11ae5b7f30530058380c7
Tiffany Wu <tiffany@twu.io>
2020-02-01 15:15:18 -0500

    fix tests

--- src/components/form/RawInput/index.tsx
+++ src/components/form/RawInput/index.tsx
@@ -11,7 +11,6 @@ type IconPosition = 'right' | 'left';
 export interface RawInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
-  iconContainerClassName?: string;
 
   /**
    * Since the most common callout is for validation errors, you shouldn't need to customize this
@@ -78,7 +77,6 @@ export interface RawInputProps
 
 function RawInput({
   className,
-  iconContainerClassName,
   errorSeverity = 'warning',
   hasError = false,
   icon,
