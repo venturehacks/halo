d52b130e943f9e4b0be38a9f9b67b594a5bc7826
Drew Lustro <drew@angel.co>
2020-02-19 19:10:25 -0800

    attempt to get remark-vscode working

--- src/gatsby-theme-docz/src/components/index.js
+++ src/gatsby-theme-docz/src/components/index.js
@@ -2,11 +2,13 @@
 import * as headings from 'gatsby-theme-docz/src/components/Headings';
 import { Layout } from 'gatsby-theme-docz/src/components/Layout';
 import { Playground } from 'gatsby-theme-docz/src/components/Playground';
+import { Pre } from 'gatsby-theme-docz/src/components/Pre';
 import { Props } from 'gatsby-theme-docz/src/components/Props';
 
 export default {
   ...headings,
   playground: Playground,
+  pre: Pre,
   layout: Layout,
   props: Props,
 };
