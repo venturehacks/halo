c91110dd9539943f65022447744d772b6ae7a46c
Drew Lustro <drewlustro@users.noreply.github.com>
2020-03-31 13:17:37 -0700

    [Span] fix muted, xmuted, xxmuted prop functionality, add snapsho… (#58)
    fixes #57

--- src/components/core/Span/__tests__/Span.test.tsx
+++ src/components/core/Span/__tests__/Span.test.tsx
@@ -3,7 +3,35 @@ import React from 'react';
 
 import { Span } from '../index';
 
-test('Smoke test', () => {
-  const span = render(<Span>Bueno</Span>);
-  expect(span.text()).toEqual('Bueno');
+describe('Span', () => {
+  test('smoke', () => {
+    const span = render(<Span>Bueno</Span>);
+    expect(span.text()).toEqual('Bueno');
+  });
+
+  describe('snapshots', () => {
+    test(`contrast`, () => {
+      const component = render(<Span contrast="AA">AA</Span>);
+
+      expect(component).toMatchSnapshot();
+    });
+
+    test(`muted`, () => {
+      const component = render(<Span muted>AAA</Span>);
+
+      expect(component).toMatchSnapshot();
+    });
+
+    test(`xmuted`, () => {
+      const component = render(<Span xmuted>AA</Span>);
+
+      expect(component).toMatchSnapshot();
+    });
+
+    test(`xxmuted`, () => {
+      const component = render(<Span xxmuted>A</Span>);
+
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
