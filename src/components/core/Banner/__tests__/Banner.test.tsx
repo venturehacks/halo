c82dc49f86502e620aa4ccbf830b64d0ebfeb43a
Drew Lustro <drew@angel.co>
2020-04-01 15:52:19 -0700

    [Banner] snapshot tests

--- src/components/core/Banner/__tests__/Banner.test.tsx
+++ src/components/core/Banner/__tests__/Banner.test.tsx
@@ -1,9 +1,45 @@
-import { shallow } from 'enzyme';
-import * as React from 'react';
+import { render, shallow } from 'enzyme';
+import React from 'react';
 
 import { Banner } from '../index';
 
-test('smoke', () => {
-  const component = shallow(<Banner>PHP</Banner>);
-  expect(component.text()).toEqual('PHP');
+describe('Banner', () => {
+  test('smoke', () => {
+    const component = shallow(<Banner>PHP</Banner>);
+    expect(component.text()).toEqual('PHP');
+  });
+
+  describe('snapshot', () => {
+    test('default', () => {
+      const component = render(<Banner />);
+      expect(component).toMatchSnapshot();
+    });
+
+    test('constrain', () => {
+      const component = render(<Banner constrain />);
+      expect(component).toMatchSnapshot();
+    });
+
+    test('offer dismiss', () => {
+      const component = render(<Banner offerDismiss />);
+      expect(component).toMatchSnapshot();
+    });
+
+    describe('variant', () => {
+      test('success', () => {
+        const component = render(<Banner variant="success" />);
+        expect(component).toMatchSnapshot();
+      });
+
+      test('warning', () => {
+        const component = render(<Banner variant="warning" />);
+        expect(component).toMatchSnapshot();
+      });
+
+      test('error', () => {
+        const component = render(<Banner variant="error" />);
+        expect(component).toMatchSnapshot();
+      });
+    });
+  });
 });
