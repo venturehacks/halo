8962f64ae306a609377bae38dc6fe2bdbef3bd68
Drew Lustro <drew@angel.co>
2020-04-02 16:39:18 -0700

    [Dialog] snapshot and interactive test suite

--- src/components/core/Dialog/styles.scss.d.ts
+++ src/components/core/Dialog/styles.scss.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "component": string;
+  readonly "contextInline": string;
   readonly "contextModal": string;
   readonly "title": string;
   readonly "paddingDefault": string;
