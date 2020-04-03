04a2c91e521d0c48d43e44b10db28bdc379a358f
Drew Lustro <drew@angel.co>
2020-04-02 17:34:51 -0700

    [LoadingIndicator] snapshot tests

--- src/components/core/LoadingIndicator/__tests__/LoadingIndicator.test.tsx
+++ src/components/core/LoadingIndicator/__tests__/LoadingIndicator.test.tsx
@@ -3,7 +3,30 @@ import React from 'react';
 
 import { LoadingIndicator } from '../index';
 
-test('smoke', () => {
-  const component = render(<LoadingIndicator />);
-  expect(component.text()).toContain('');
+describe('LoadingIndicator', () => {
+  test('smoke', () => {
+    const component = render(<LoadingIndicator />);
+    expect(component.text()).toContain('');
+  });
+
+  describe('snapshot', () => {
+    test('primary', () => {
+      const component = render(<LoadingIndicator variant="primary" bare />);
+
+      expect(component).toMatchSnapshot();
+    });
+
+    test('alternate', () => {
+      const component = render(<LoadingIndicator variant="alternate" bare />);
+
+      expect(component).toMatchSnapshot();
+    });
+
+    test('default component wrapping', () => {
+      // should wrap in div.component
+      const component = render(<LoadingIndicator variant="alternate" />);
+
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
