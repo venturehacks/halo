fd365d50a7c5ccd28602cdad9ba9dda122008377
Drew Lustro <drew@angel.co>
2020-07-01 15:12:44 -0700

    use docker via buildkite pipeline.rb

---
+++ .buildkite/pipeline.rb
@@ -0,0 +1,15 @@
+require 'buildkite_utils'
+
+STEPS << BuildkiteUtils.k8s_step(
+  step: :test,
+  name: :halo,
+  image: 'node:10',
+  timeout_in_minutes: 20,
+  command: <<-EOF
+    yarn install
+    yarn test
+    yarn run build
+  EOF
+)
+
+BuildkiteUtils.dump!(steps: STEPS)
