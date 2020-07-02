0257c9b0c8e5bd4f6c631a307084768b7a329d62
korbin <k1@k1.io>
2020-07-02 13:37:54 -0600

    add command to pipeline

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -6,6 +6,7 @@ STEPS << BuildkiteUtils.build_step(name: NAME, target: :build)
 STEPS << BuildkiteUtils.k8s_step(
   step: :test,
   name: NAME,
+  command: 'yarn test',
   depends_on: "build:test:#{NAME}",
   image: BuildkiteUtils.image(NAME, :build),
 )
