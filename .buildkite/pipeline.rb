987ee4915e2939d85afb89ddfe3d260a61b9fa70
korbin <k1@k1.io>
2020-07-02 14:10:06 -0600

    fix depends_on

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -6,8 +6,7 @@ STEPS << BuildkiteUtils.build_step(name: NAME, target: :build)
 STEPS << BuildkiteUtils.k8s_step(
   step: :test,
   name: NAME,
-  command: 'yarn test',
-  depends_on: "build:test:#{NAME}",
+  depends_on: "build:build:#{NAME}",
   image: BuildkiteUtils.image(NAME, :build),
   timeout_in_minutes: 20,
   resources: { requests: { cpu: '2', memory: '2Gi' }, limits: { cpu: '2', memory: '2Gi' } },
