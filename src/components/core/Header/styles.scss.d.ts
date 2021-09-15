cc6821f044060596891e3fe3389805d7cd595109
Drew Lustro <drew@angel.co>
2021-09-14 19:12:55 -0700

    :package: remove awesome-typed-sass-modules

--- src/components/core/Header/styles.scss.d.ts
+++
@@ -1,7 +0,0 @@
-declare const styles: {
-  readonly "component": string;
-  readonly "flow": string;
-  readonly "inlineBlock": string;
-};
-export = styles;
-
--- src/components/form/FieldErrorMessage/styles.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "component": string;
-  readonly "text-only": string;
-  readonly "warning": string;
-  readonly "critical": string;
-  readonly "block": string;
-};
-export = styles;
-
