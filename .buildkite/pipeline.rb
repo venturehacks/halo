d8a75121773ff821adc5d41d909d306507f00a68
Maxime Lagresle <maxime@angel.co>
2021-09-29 15:45:12 +0200

    [lev-791] use arm for ci/cd

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -2,15 +2,16 @@ require 'buildkite_utils'
 
 NAME = :halo
 STEPS = []
-STEPS << BuildkiteUtils.build_step(name: NAME, target: :build)
+STEPS << BuildkiteUtils.build_step(name: NAME, target: :build, arch: :arm64)
 STEPS << BuildkiteUtils.k8s_step(
   step: :test,
   name: NAME,
   command: 'yarn test:ci',
   depends_on: "build:build:#{NAME}",
-  image: BuildkiteUtils.image(NAME, :build),
+  image: BuildkiteUtils.image(NAME, :build, arch: :arm64),
   timeout_in_minutes: 20,
   resources: { requests: { cpu: '2', memory: '4Gi' }, limits: { cpu: '2', memory: '4Gi' } },
+  node_selector: { pool: :test, 'kubernetes.io/arch' => 'arm64' },
 )
 
 BuildkiteUtils.dump!(steps: STEPS)
