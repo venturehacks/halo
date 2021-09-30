c3669a16dc69d31a6423c3e2827d7dc59396e69b
Maxime Lagresle <maxime@angel.co>
2021-09-30 11:55:18 +0200

    fix build key

--- .buildkite/pipeline.rb
+++ .buildkite/pipeline.rb
@@ -7,7 +7,7 @@ STEPS << BuildkiteUtils.k8s_step(
   step: :test,
   name: NAME,
   command: 'yarn test:ci',
-  depends_on: "build:build:#{NAME}",
+  depends_on: "build:build:#{NAME}:arm64",
   image: BuildkiteUtils.image(NAME, :build, arch: :arm64),
   timeout_in_minutes: 20,
   resources: { requests: { cpu: '2', memory: '4Gi' }, limits: { cpu: '2', memory: '4Gi' } },
