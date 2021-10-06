65668be675e42cea0ae7915fa0bfac7ac16e188c
Drew Lustro <drew@angel.co>
2021-10-06 16:45:54 -0700

    Fix #77 - Flair 3xs size

--- src/components/core/Flair/index.tsx
+++ src/components/core/Flair/index.tsx
@@ -19,7 +19,7 @@ export interface FlairProps {
   /**
    * @default sm
    */
-  size?: Extract<TextSize, '2xs' | 'xs' | 'sm' | 'md' | 'lg'>;
+  size?: Extract<TextSize, '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'> | '3xs';
 
   /**
    * Label text transform.
@@ -41,6 +41,9 @@ function FlairRaw({
     className,
     textTransform === 'uppercase' && styles.uppercase,
     styles[color],
+    // NOTE(drew): 3xs size is not officially in Halo, but
+    // it is useful for this type of element
+    size === '3xs' && styles.size_3xs,
     size === '2xs' && styles.size_2xs,
     size === 'xs' && styles.size_xs,
     size === 'sm' && styles.size_sm,
