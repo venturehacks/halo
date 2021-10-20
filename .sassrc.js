a21a6763dc6eb6ca64e85e7452e56dc9ef2b798f
Drew Lustro <drew@angel.co>
2021-10-20 14:19:51 -0700

    Revert ":knife: unused config file"
    This reverts commit cb3a3dcab0767a71938c8884bf9c0dc9d928b4d7.

---
+++ .sassrc.js
@@ -0,0 +1,9 @@
+/**
+ * Provide `atsm` custom includePaths
+ */
+const path = require('path');
+const CWD = process.cwd();
+
+module.exports = {
+  includePaths: [path.resolve(CWD, 'scss'), path.resolve(CWD, 'node_modules')],
+};
