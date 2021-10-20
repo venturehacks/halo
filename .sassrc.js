cb3a3dcab0767a71938c8884bf9c0dc9d928b4d7
Drew Lustro <drew@angel.co>
2021-10-19 17:34:36 -0700

    :knife: unused config file

--- .sassrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * Provide `atsm` custom includePaths
- */
-const path = require('path');
-const CWD = process.cwd();
-
-module.exports = {
-  includePaths: [path.resolve(CWD, 'scss'), path.resolve(CWD, 'node_modules')],
-};
