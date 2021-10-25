defa5ec438a90c64493fb50b7908ed9257c86721
Drew Lustro <drew@angel.co>
2021-10-25 16:30:08 -0700

    push bundle size metrics to datadog

--- .size-limit.js
+++ .size-limit.js
@@ -1,12 +1,12 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 module.exports = [
   {
-    name: 'CommonJS module',
+    name: 'CommonJS',
     path: ['packages/halo/dist/halo.js', 'packages/halo/dist/halo.css'],
     limit: '100 kB',
   },
   {
-    name: 'ES6 module',
+    name: 'ES6',
     path: [
       'packages/halo/dist/halo.module.js',
       'packages/halo/dist/halo.module.css',
@@ -17,5 +17,9 @@ module.exports = [
     name: 'Typings',
     path: ['packages/halo/dist/typings/**/*.d.ts'],
     limit: '15 kB',
+  },
+  {
+    name: 'Total',
+    path: ['packages/halo/dist/**/*'],
   },
 ];
---
+++ bin/ci/datadog-size-limit.ts
@@ -0,0 +1,50 @@
+import { v1 } from '@datadog/datadog-api-client';
+// tslint:disable-next-line: no-submodule-imports
+import { Series } from '@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/Series';
+import fs from 'fs';
+import slugify from 'slugify';
+
+interface SizeLimitEntry {
+  name: string;
+  passed: boolean;
+  size: number;
+}
+
+const BUILDKITE_BRANCH = process.env.BUILDKITE_BRANCH || 'master';
+const bundleSizeData = fs.readFileSync('datadog-size-limit.json', {
+  encoding: 'utf-8',
+});
+const bundleSize: SizeLimitEntry[] = JSON.parse(bundleSizeData);
+const now = Math.floor(Date.now() / 1000);
+
+const series: Series[] = bundleSize.map(({ name, passed, size }) => ({
+  host: 'buildkite.halo',
+  metric: `build.metric.bundle.size`,
+  type: 'count',
+  points: [[now, size]],
+  tags: [
+    `branch:${BUILDKITE_BRANCH}`,
+    `category:${slugify(name).toLowerCase()}`,
+    `passed:${passed}`,
+  ],
+}));
+
+const configuration = v1.createConfiguration();
+const apiInstance = new v1.MetricsApi(configuration);
+
+const params: v1.MetricsApiSubmitMetricsRequest = {
+  body: {
+    series,
+  },
+};
+
+apiInstance
+  .submitMetrics(params)
+  .then((data: any) => {
+    // tslint:disable-next-line: no-console
+    console.log('✅ Pushed metrics to datadog.');
+    // tslint:disable-next-line: no-console
+    console.log(JSON.stringify(data));
+  })
+  // tslint:disable-next-line: no-console
+  .catch((error: any) => console.error(error));
