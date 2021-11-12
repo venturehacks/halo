d8d0f9e3c8b0a86c8d5e96d84d99901c692e0f9a
Drew Lustro <drew@angel.co>
2021-11-11 18:50:32 -0800

    :taco: First class support for SVGs!

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,23 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
-
 const presets = ['babel-preset-gatsby', '@babel/preset-typescript'];
+
+const svgoConfig = require('./svgo.config.js');
+
+const plugins = [['inline-react-svg', { svgo: svgoConfig }]];
 
 module.exports = {
   env: {
     development: {
       presets,
+      plugins,
     },
     production: {
       presets,
+      plugins,
     },
     test: {
       presets,
+      plugins,
     },
   },
 };
