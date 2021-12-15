b8185b3d89ae742b2563c43033e774b1fad3963a
Drew Lustro <drewlustro@users.noreply.github.com>
2021-12-15 11:42:39 -0800

    [LEV-1141] Bracket (#120)

---
+++ packages/halo/src/components/structure/Bracket/__tests__/Bracket.test.tsx
@@ -0,0 +1,79 @@
+import { render } from 'enzyme';
+import React from 'react';
+
+import { Bracket } from '../index';
+
+describe('Bracket', () => {
+  test('smoke', () => {
+    const component = render(
+      <Bracket>
+        <span>Label</span>
+        <button>Click me</button>
+      </Bracket>,
+    );
+    expect(component.text()).toContain('LabelClick me');
+    expect(component.hasClass('isolateFirstChild')).toBeTruthy();
+  });
+
+  test('className', () => {
+    const component = render(
+      <Bracket className="mb-10">
+        <span>Label</span>
+        <button>Click me</button>
+      </Bracket>,
+    );
+    expect(component.hasClass('mb-10')).toBeTruthy();
+  });
+
+  test('gap', () => {
+    const component = render(
+      <Bracket gap="8">
+        <span>Label</span>
+        <button>Click me</button>
+      </Bracket>,
+    );
+    expect(component.hasClass('gap-8')).toBeTruthy();
+  });
+
+  test('isolate last child', () => {
+    const component = render(
+      <Bracket isolate="last-child">
+        <span>Label</span>
+        <button>Click me</button>
+      </Bracket>,
+    );
+    expect(component.hasClass('isolateLastChild')).toBeTruthy();
+  });
+
+  describe('snapshot', () => {
+    test('smoke', () => {
+      const component = render(
+        <Bracket>
+          <span>Label</span>
+          <button>Click me</button>
+        </Bracket>,
+      );
+      expect(component).toMatchSnapshot();
+    });
+
+    test('gap', () => {
+      const component = render(
+        <Bracket gap="6">
+          <span>Label</span>
+          <button>Click me</button>
+        </Bracket>,
+      );
+      expect(component).toMatchSnapshot();
+    });
+
+    test('isolate last child', () => {
+      const component = render(
+        <Bracket isolate="last-child">
+          <span>Label</span>
+          <button>Click me</button>
+        </Bracket>,
+      );
+      expect(component).toMatchSnapshot();
+    });
+  });
+});
