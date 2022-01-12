d53a7eedb5fdcd18888ea680c2e33f4eb3d02ba6
Vishal Jeet <vishal@angel.co>
2022-01-12 05:25:43 -0500

    remove styles.module.scss and use tailwind instead + update snapshots

--- packages/halo/src/components/core/Badge/index.tsx
+++ packages/halo/src/components/core/Badge/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 
 import { Tooltip } from '../Tooltip';
 
-import styles from './styles.module.scss';
-
 export type BadgeColor =
   | 'blue'
   | 'gray'
@@ -38,20 +36,29 @@ function Badge({
   size = 'sm',
   tooltip,
 }: BadgeProps) {
-  const component = (
-    <div
-      className={classNames(
-        styles.component,
-        styles[color],
-        styles[position],
-        styles[shape],
-        styles[size],
-        className,
-      )}
-    >
-      {children}
-    </div>
+  const classnames = classNames(
+    className,
+    'absolute',
+    'right-0',
+    'border-solid border-slate-100',
+    'text-center text-3xs font-bold uppercase',
+    'p-1',
+    color === 'gray' && 'bg-slate-600 border-slate-100 text-white',
+    color === 'purple' && 'bg-purple-600 border-purple-100 text-white',
+    color === 'blue' && 'bg-blue-600 border-blue-100 text-white',
+    color === 'green' && 'bg-green-600 border-green-100 text-white',
+    color === 'red' && 'bg-red-600 border-red-100 text-white',
+    color === 'orange' && 'bg-orange-200 border-orange-200 text-orange-600',
+    shape === 'circle' && 'rounded-full',
+    shape === 'square' && 'rounded-md',
+    size === 'sm' && 'h-4 min-w-4 leading-extra-tight',
+    size === 'md' && 'h-6 min-w-6 pt-1',
+    position === 'bottom' && 'bottom-1',
+    position === 'top' && size === 'sm' && '-top-1',
+    position === 'top' && size === 'md' && '-top-2',
   );
+
+  const component = <div className={classnames}>{children}</div>;
 
   return tooltip ? <Tooltip content={tooltip}>{component}</Tooltip> : component;
 }
