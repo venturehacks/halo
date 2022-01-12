a619474bcbb09cb363799d5267857c8b3ed056e7
Vishal Jeet <vishal@angel.co>
2022-01-12 03:27:26 -0500

    [LEV-1145] use Tailwind to style AttentionIndicator

--- packages/halo/src/components/core/AttentionIndicator/index.tsx
+++ packages/halo/src/components/core/AttentionIndicator/index.tsx
@@ -1,8 +1,5 @@
 import classNames from 'classnames';
-import { camelCase } from 'lodash';
 import React from 'react';
-
-import styles from './styles.module.scss';
 
 export type AttentionIndicatorColor = 'orange' | 'red';
 
@@ -38,23 +35,38 @@ function AttentionIndicator({
     displayString = '99+';
   }
 
-  const shapeClass = styles[camelCase(shape)];
+  const componentClasses = classNames(
+    className,
+    'absolute',
+    'leading-none',
+    'p-0.5',
+    'right-0',
+    'text-center',
+    'text-white',
+    'top-0',
+    color === 'red' && 'bg-red-600',
+    color === 'orange' && 'bg-orange-600',
+    size === 'sm' && 'h-3 min-w-3 text-4xs',
+    size === 'md' && 'h-4 min-w-4 text-3xs font-bold',
+    shape === 'circle' && 'rounded-full',
+    shape === 'rounded-rectangle' && 'font-bold leading-normal',
+    shape === 'rounded-rectangle' && size === 'sm' && 'rounded-md h-4 pt-0.5',
+    shape === 'rounded-rectangle' && size === 'md' && 'rounded-lg h-5',
+  );
+
+  const iconClasses = classNames(
+    className,
+    'block',
+    'm-auto',
+    size === 'sm' && shape === 'circle' && 'h-1.5 w-1.5 pt-px',
+    size === 'sm' && shape === 'rounded-rectangle' && 'h-2 w-2 pt-0.5',
+    size === 'md' && shape === 'circle' && 'h-2 w-2 pt-0.5',
+    size === 'md' && shape === 'rounded-rectangle' && 'h-2.5 w-2.5 pt-0.75',
+  );
 
   return (
-    <div
-      className={classNames(
-        styles.component,
-        styles[color],
-        styles[size],
-        shapeClass,
-        className,
-      )}
-    >
-      {icon && (
-        <div className={classNames(styles.icon, styles[size], shapeClass)}>
-          {icon}
-        </div>
-      )}
+    <div className={componentClasses}>
+      {icon && <div className={iconClasses}> {icon} </div>}
       {displayString && <>{displayString}</>}
     </div>
   );
