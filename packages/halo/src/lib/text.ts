973155124d1bff7be991e62407e291c67ecbbf66
Drew Lustro <drew@angel.co>
2022-01-25 16:47:50 -0800

    typography interface revision

--- packages/halo/src/lib/text.ts
+++ packages/halo/src/lib/text.ts
@@ -2,13 +2,7 @@ export type TextContrast = 'A' | 'AA' | 'AAA' | 'AAAA';
 
 export type TextColorScheme = 'dark' | 'light';
 
-export type TextWeight =
-  | 'ultralight'
-  | 'light'
-  | 'regular'
-  | 'medium'
-  | 'semibold'
-  | 'bold';
+export type TextWeight = 'light' | 'regular' | 'medium' | 'semibold' | 'bold';
 
 export type TextSize =
   | '6xl'
@@ -23,25 +17,36 @@ export type TextSize =
   | 'xs'
   | '2xs';
 
-export type TextLineHeight = 'default' | 'comfortable' | 'compact' | 'dense';
+export type TextLineHeight = 'default' | 'comfortable' | 'compact';
 
 export interface TextContrastProps {
+  colorScheme?: TextColorScheme;
   contrast?: TextContrast;
   muted?: boolean;
   xmuted?: boolean;
   xxmuted?: boolean;
 }
 
+// TODO(drew): force inclusion of these rules so we can remove
+// these conditional expressions (save kb)
 export function textContrastForConfiguration({
   muted,
   xmuted,
   xxmuted,
   contrast,
-}: TextContrastProps): TextContrast {
-  if (contrast) return contrast;
-  if (xxmuted) return 'A';
-  if (xmuted) return 'AA';
-  if (muted) return 'AAA';
+  colorScheme = 'dark',
+}: TextContrastProps) {
+  if (colorScheme === 'dark') {
+    if (contrast) return `text-dark-${contrast.toLowerCase()}`;
+    if (xxmuted) return 'text-dark-a';
+    if (xmuted) return 'text-dark-aa';
+    if (muted) return 'text-dark-aaa';
+    return 'text-dark-aaaa';
+  }
 
-  return 'AAAA';
+  if (contrast) return `text-light-${contrast.toLowerCase()}`;
+  if (xxmuted) return 'text-light-a';
+  if (xmuted) return 'text-light-aa';
+  if (muted) return 'text-light-aaa';
+  return 'text-light-aaaa';
 }
