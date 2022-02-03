db2110ab44996793b15b661c7ce91bf8dbdde502
Vishal Jeet <vishal@angel.co>
2022-02-03 01:24:13 -0500

    update API to be rigid, with extendible flexibility

--- packages/halo/src/components/core/AvatarLockup/index.tsx
+++ packages/halo/src/components/core/AvatarLockup/index.tsx
@@ -5,47 +5,86 @@ import {
   ForwardedRefProps,
   withForwardedRef,
 } from '../../../lib/withForwardedRef';
+import { Box } from '../../structure/Box';
 import { Avatar } from '../Avatar';
 
 export type AvatarLockupOrientation = 'left' | 'right' | 'top';
 
 export type AvatarLockupSize = 'sm' | 'md' | 'lg';
 
-export interface AvatarLockupProps {
-  children: React.ReactNode;
+export interface AvatarLockupProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * Optional byline to be displayed below the title. Mututally exclusive with children.
+   */
+  byline?: string;
+  /**
+   * Can be used to override the content to be placed next to the avatar. Should be used in the
+   * case where there is more than just a title/byline.
+   */
+  children?: React.ReactNode;
   className?: string;
   imageUrl: string;
   orientation?: AvatarLockupOrientation;
   size?: AvatarLockupSize;
+  /**
+   * Optional title to be placed next to the avatar. Mututally exclusive with children.
+   */
+  title?: string;
 }
 
 function AvatarLockupRaw({
   className,
+  forwardedRef,
+  byline,
   children,
   imageUrl,
   orientation = 'left',
   size = 'md',
-  forwardedRef,
+  title,
   ...rest
-}: AvatarLockupProps &
-  ForwardedRefProps<HTMLDivElement> &
-  React.HTMLAttributes<HTMLDivElement>) {
-  const classnames = classNames(
+}: AvatarLockupProps & ForwardedRefProps<HTMLDivElement>) {
+  const componentClassnames = classNames(
     className,
     'flex gap-2 text-left',
     (orientation === 'left' || orientation === 'right') &&
       'flex-row items-center',
     orientation === 'top' && 'flex-col',
-    size === 'sm' && 'gap-1 text-sm',
-    size === 'md' && 'gap-3 text-md font-medium',
-    size === 'lg' && 'gap-4 text-lg font-medium',
+    size === 'sm' && 'gap-1',
+    size === 'md' && 'gap-3',
+    size === 'lg' && 'gap-4',
+  );
+  const titleClassnames = classNames(
+    size === 'sm' && 'text-sm font-normal',
+    size === 'md' && 'text-md font-medium',
+    size === 'lg' && 'text-lg font-medium',
+  );
+  const bylineClassnames = classNames(
+    size === 'sm' && 'text-xs font-light',
+    size === 'md' && 'text-sm font-light',
+    size === 'lg' && 'text-md font-normal',
+  );
+
+  // should either render the children OR title + byline, not both.
+  const hasChildren = children && !byline && !title;
+
+  const content = (
+    <>
+      {hasChildren && children}
+      {!hasChildren && (
+        <Box>
+          <div className={titleClassnames}>{title}</div>
+          <div className={bylineClassnames}>{byline}</div>
+        </Box>
+      )}
+    </>
   );
 
   return (
-    <div className={classnames} {...rest}>
-      {orientation === 'right' && children}
+    <div className={componentClassnames} {...rest}>
+      {orientation === 'right' && content}
       <Avatar imageUrl={imageUrl} size={size} />
-      {orientation !== 'right' && children}
+      {orientation !== 'right' && content}
     </div>
   );
 }
