e622b19b9f1ea840ffc3b6f41238c08c9b3c49ca
Vishal Jeet <vishal@angel.co>
2022-02-07 16:10:07 -0500

    try more options for parsing css

--- packages/halo/codemod/utils/sass.js
+++ packages/halo/codemod/utils/sass.js
@@ -1,13 +1,30 @@
 const fs = require('fs');
 const postcss = require('postcss');
+import { parse } from 'css-tree';
 
 function getCssForClass(fileName, className) {
   console.log(`Getting css code for: ${fileName}`);
-  const root = postcss.parse(fs.readFileSync(fileName));
+  const root = postcss.parse(fs.readFileSync(fileName)).toString();
 
+  const parsed = parse(root);
   const value = 'qewr';
+  const cssSelectors = extractCssSelectors(root.nodes);
 
   return root;
+}
+
+function extractCssSelectors(nodes) {
+  if (!nodes || nodes.length === 0) {
+    return [];
+  }
+
+  const ruleNodes = nodes.filter(node => node.type === 'rule');
+  let selectors = ruleNodes.map(r => r.selector);
+  ruleNodes.forEach(ruleNode => {
+    selectors = selectors.concat(extractCssSelectors(ruleNode.nodes));
+  });
+
+  return selectors;
 }
 
 export { getCssForClass };
