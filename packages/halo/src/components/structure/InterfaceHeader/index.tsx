5e406bfbb2f5168321d7e072160d9d4204a5a746
Drew Lustro <drew@angel.co>
2022-02-09 13:06:24 -0800

    [InterfaceHeader] revise hierarchy, remove `children`

--- packages/halo/src/components/structure/InterfaceHeader/index.tsx
+++ packages/halo/src/components/structure/InterfaceHeader/index.tsx
@@ -6,12 +6,11 @@ import {
   withForwardedRef,
 } from '../../../lib/withForwardedRef';
 
-type InterfaceHeaderLevel = 'page' | 'panel' | 'section' | 'micro';
+export type InterfaceHeaderLevel = 'page' | 'section' | 'subdivision' | 'mini';
 
 /* eslint-disable typescript-sort-keys/interface */
 export interface InterfaceHeaderProps
   extends React.HTMLAttributes<HTMLHeadingElement> {
-  children: React.ReactNode;
   className?: string;
   level?: InterfaceHeaderLevel;
   /**
@@ -25,55 +24,83 @@ export interface InterfaceHeaderProps
    */
   page?: boolean;
   /**
-   * Optional shorthand for level="panel"
+   * Optional shorthand for level="subdivision"
    * @default false
    */
-  panel?: boolean;
+  subdivision?: boolean;
   /**
    * Optional shorthand for level="section"
    * @default false
    */
   section?: boolean;
   /**
-   * Optional shorthand for level="micro"
+   * Optional shorthand for level="mini"
    * @default false
    */
-  micro?: boolean;
+  mini?: boolean;
+  title: string;
+  byline?: string;
 }
 /* eslint-enable typescript-sort-keys/interface */
 
 function InterfaceHeaderRaw({
   className,
-  children,
   level,
   page,
-  panel,
   section,
-  micro,
+  subdivision,
+  mini,
   flow,
   forwardedRef,
+  title,
+  byline,
   ...rest
 }: InterfaceHeaderProps & ForwardedRefProps<HTMLHeadingElement>) {
-  if (!level && !page && !panel && !section && !micro) {
+  if (!level && !page && !section && !subdivision && !mini) {
     throw new Error('[Halo InterfaceHeader] must specify `level`');
   }
+
+  const isPage = page || level === 'page';
+  const isSection = section || level === 'section';
+  const isSubdivision = subdivision || level === 'subdivision';
+  const isMini = mini || level === 'mini';
 
   return (
     <header
       ref={forwardedRef}
       className={classNames(
-        (page || level === 'page') && 'text-xl text-slate-800',
-        (panel || level === 'panel') && 'text-lg text-dark-aaa',
-        (section || level === 'section') && 'text-md text-dark-aaa',
-        (micro || level === 'micro') && 'text-xs text-dark-aa',
-        'font-medium',
-        'antialiased',
-        flow && 'mb-4',
+        'text-dark-aaaa font-medium antialiased',
+        isPage && 'text-xl',
+        isSection && 'text-lg',
+        isSubdivision && 'text-md',
+        isMini && 'text-sm',
+        flow && isPage && 'mb-6',
+        flow && isSection && 'mb-4',
+        flow && isSubdivision && 'mb-4',
+        flow && isMini && 'mb-4',
         className,
       )}
       {...rest}
     >
-      {children}
+      {byline ? (
+        <>
+          <div>{title}</div>
+          <div
+            className={classNames(
+              'text-dark-aa',
+              'font-normal',
+              isPage && 'text-md mt-2',
+              isSection && 'text-md mt-1',
+              isSubdivision && 'text-sm mt-0.5',
+              isMini && 'text-xs',
+            )}
+          >
+            {byline}
+          </div>
+        </>
+      ) : (
+        title
+      )}
     </header>
   );
 }
