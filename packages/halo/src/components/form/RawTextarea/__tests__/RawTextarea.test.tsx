ab2e9c2f92fab8c6bfbed92028cdec70bfe92a87
Drew Lustro <drew@angel.co>
2022-02-24 12:05:27 -0800

    [RawTextarea] refresh, add snapshot test suite

--- packages/halo/src/components/form/RawTextarea/__tests__/RawTextarea.test.tsx
+++ packages/halo/src/components/form/RawTextarea/__tests__/RawTextarea.test.tsx
@@ -3,10 +3,37 @@ import React from 'react';
 
 import { RawTextarea } from '../index';
 
-test('smoke', () => {
-  const component = render(
-    <RawTextarea onChange={() => {}} value="Current value" />,
-  );
+describe('RawTextarea', () => {
+  test('smoke', () => {
+    const component = render(
+      <RawTextarea onChange={() => {}} value="textarea value" />,
+    );
 
-  expect(component.text()).toContain('Current value');
+    expect(component.text()).toContain('textarea value');
+  });
+
+  describe('snapshot', () => {
+    describe('size', () => {
+      test('sm', () => {
+        const component = render(
+          <RawTextarea onChange={() => {}} size="sm" value="small" />,
+        );
+        expect(component).toMatchSnapshot();
+      });
+
+      test('md', () => {
+        const component = render(
+          <RawTextarea onChange={() => {}} value="medium" />,
+        );
+        expect(component).toMatchSnapshot();
+      });
+
+      test('lg', () => {
+        const component = render(
+          <RawTextarea onChange={() => {}} size="lg" value="large" />,
+        );
+        expect(component).toMatchSnapshot();
+      });
+    });
+  });
 });
--- packages/halo/src/components/form/RawTextarea/index.tsx
+++ packages/halo/src/components/form/RawTextarea/index.tsx
@@ -1,28 +1,22 @@
 import classNames from 'classnames';
 import React from 'react';
 
-import { FORM_FIELD_ERROR_IDENTIFIER } from '../../../lib';
+import {
+  FormInputIntrinsicWidth,
+  RawInputBase,
+  rawInputClassNames,
+} from '../RawInput';
 
 import styles from './styles.module.scss';
 
 export interface RawTextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  extends RawInputBase,
+    React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   className?: string;
   /**
-   * Since the most common callout is for validation errors, you shouldn't need to customize this.
-   * @default warning
-   */
-  errorSeverity?: 'warning' | 'critical';
-  /**
-   * Call out element that needs attention
-   * @default false
-   */
-  hasError?: boolean;
-  /**
-   * Default width
    * @default auto
    */
-  intrinsicWidth?: 'auto' | '100%';
+  intrinsicWidth?: FormInputIntrinsicWidth;
 
   /**
    * Field name
@@ -40,23 +34,25 @@ export interface RawTextareaProps
 
 function RawTextarea({
   className,
-  intrinsicWidth,
+  intrinsicWidth = 'auto',
   hasError = false,
   errorSeverity = 'warning',
   transparent = false,
+  size = 'md',
   ...rest
 }: RawTextareaProps) {
   return (
     <textarea
       className={classNames(
         styles.component,
+        rawInputClassNames({
+          errorSeverity,
+          hasError,
+          intrinsicWidth,
+          size,
+          transparent,
+        }),
         className,
-        transparent ? styles.transparent : styles.bordered,
-        intrinsicWidth === '100%' && styles.width100,
-        hasError && styles.hasError,
-        hasError && FORM_FIELD_ERROR_IDENTIFIER,
-        errorSeverity === 'warning' && styles.warning,
-        errorSeverity === 'critical' && styles.critical,
       )}
       {...rest}
     />
