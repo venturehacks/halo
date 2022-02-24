7bed22855bf3aa56719b500459a8bd4311f037fd
Drew Lustro <drew@angel.co>
2022-02-23 17:04:54 -0800

    [RawSelect] expand test suite

--- packages/halo/src/components/form/RawSelect/__tests__/RawSelect.test.tsx
+++ packages/halo/src/components/form/RawSelect/__tests__/RawSelect.test.tsx
@@ -1,17 +1,37 @@
 import { render } from 'enzyme';
 import React from 'react';
 
-import { RawSelect } from '../index';
+import { RawSelect, RawSelectOption } from '../index';
 
-test('smoke', () => {
-  const component = render(
-    <RawSelect
-      options={[
-        { label: 'Foo', value: 'foo ' },
-        { label: 'Bar', value: 'bar ' },
-      ]}
-    />,
-  );
+const options: RawSelectOption[] = [
+  { label: 'Chocolate', value: 'chocolate' },
+  { label: 'Vanilla', value: 'vanilla' },
+  { label: 'Strawberry', value: 'strawberry' },
+];
 
-  expect(component.text()).toContain('Foo');
+describe('RawSelect', () => {
+  test('smoke', () => {
+    const component = render(<RawSelect options={options} />);
+
+    expect(component.text()).toContain('ChocolateVanillaStrawberry');
+  });
+
+  describe('snapshot', () => {
+    describe('size', () => {
+      test('sm', () => {
+        const component = render(<RawSelect options={options} size="sm" />);
+        expect(component).toMatchSnapshot();
+      });
+
+      test('md', () => {
+        const component = render(<RawSelect options={options} />);
+        expect(component).toMatchSnapshot();
+      });
+
+      test('lg', () => {
+        const component = render(<RawSelect options={options} size="lg" />);
+        expect(component).toMatchSnapshot();
+      });
+    });
+  });
 });
