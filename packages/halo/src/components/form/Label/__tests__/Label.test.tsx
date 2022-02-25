a945ff53c01588d3f92981f2d45281fbb639cb14
Drew Lustro <drew@angel.co>
2022-02-24 16:36:53 -0800

    [Label] expand test suite, add snapshots

--- packages/halo/src/components/form/Label/__tests__/Label.test.tsx
+++ packages/halo/src/components/form/Label/__tests__/Label.test.tsx
@@ -1,13 +1,59 @@
 import { render } from 'enzyme';
 import React from 'react';
 
+import { RawInput } from '../../RawInput';
+import { RawRadio } from '../../RawRadio';
 import { Label } from '../index';
 
-test('smoke', () => {
-  const component = render(
-    <Label supportingText="Who are you?" title="Name" />,
-  );
+describe('Label', () => {
+  test('smoke', () => {
+    const component = render(<Label byline="Who are you?" title="Name" />);
 
-  expect(component.text()).toContain('Name');
-  expect(component.text()).toContain('Who are you?');
+    expect(component.text()).toContain('Name');
+    expect(component.text()).toContain('Who are you?');
+  });
+
+  test('field group without containsFieldGroup throws', () => {
+    expect(() =>
+      render(
+        <Label byline="Who are you?" title="Name">
+          <RawRadio
+            id="chocolate"
+            label="Chocolate"
+            onChange={() => {}}
+            checked
+          />
+          <RawRadio id="vanilla" label="Vanilla" />
+          <RawRadio id="strawberry" label="Strawberry" />
+        </Label>,
+      ),
+    ).toThrowError();
+  });
+
+  describe('snapshot', () => {
+    test('standard', () => {
+      const component = render(
+        <Label byline="Who are you?" title="Name">
+          <RawInput />
+        </Label>,
+      );
+      expect(component).toMatchSnapshot();
+    });
+
+    test('containsFieldGroup', () => {
+      const component = render(
+        <Label byline="Who are you?" title="Name" containsFieldGroup>
+          <RawRadio
+            id="chocolate"
+            label="Chocolate"
+            onChange={() => {}}
+            checked
+          />
+          <RawRadio id="vanilla" label="Vanilla" />
+          <RawRadio id="strawberry" label="Strawberry" />
+        </Label>,
+      );
+      expect(component).toMatchSnapshot();
+    });
+  });
 });
