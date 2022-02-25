3b5ca6be11b1eee19b4c0840e3d37a6789697927
Drew Lustro <drew@angel.co>
2022-02-25 14:28:04 -0800

    :package: fix svgo version incompatibility, resolutions
    error: Cannot read property 'replace' of undefined during jest SVG transform

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ const presets = ['babel-preset-gatsby', '@babel/preset-typescript'];
 
 const svgoConfig = require('./svgo.config.js');
 
-// NOTE(drew): Required for Jest.
+// NOTE(drew): SVG transform required for Jest.
 // Please note that Gatsby does not leverage this transform.
 // Gatsby uses @svgr/webpack (see gatsby-node.js)
 const plugins = [['inline-react-svg', { svgo: svgoConfig }]];
