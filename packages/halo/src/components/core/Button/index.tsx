477b36f07bfec8185ee9cb5f1ffe79be6470d2e8
Vishal Jeet <vishal@angel.co>
2022-03-29 21:23:18 -0400

    fix typo, move variant_css into a map

--- packages/halo/src/components/core/Button/index.tsx
+++ packages/halo/src/components/core/Button/index.tsx
@@ -20,6 +20,36 @@ export type ButtonVariant =
   | 'gtm-clear'
   | 'gtm-error';
 
+const VARIANT_CSS: Record<ButtonVariant, string> = {
+  'primary': `bg-dark-brand border-dark-brand text-white font-medium
+              disabled:bg-blue-300 disabled:border-blue-300
+              hover:bg-blue-600 hover:border-blue-600`,
+  'gtm-primary': `bg-black border-black text-white font-normal
+                  disabled:bg-gray-400 disabled:border-gray-400 disabled:text-gray-700
+                  hover:bg-gtmblue-500 hover:border-gtmblue-500`,
+  'secondary': `bg-white border-blue-500 text-blue-500 font-medium
+                disabled:bg-white disabled:border-blue-300 disabled:text-blue-300
+                hover:bg-slate-100`,
+  'gtm-secondary': `bg-white border-black text-black font-normal
+                    disabled:bg-white disabled:border-gray-700 disabled:text-gray-700
+                    hover:bg-gtmblue-100 hover:border-gtmblue-500 hover:text-gtmblue-500`,
+  'gray': `bg-gray-100 border-dark-aaa text-dark-aaa font-medium
+           disabled:bg-gray-100 disabled:border-dark-a disabled:text-dark-a
+           hover:bg-slate-200`,
+  'gtm-gray': `bg-gray-200 border-black text-black font-normal hover:bg-gray-400
+               disabled:bg-gray-100 disabled:border-gray-500 disabled:text-gray-500`,
+  'error': `bg-red-100 border-red-100 text-light-error font-medium
+            disabled:border-gray-100 disabled:text-gray-500 disabled:bg-gray-100
+            hover:bg-red-200`,
+  'gtm-error': `bg-gtmpink-100 border-gtmpink-100 text-gtmpink-600 font-formal
+                disabled:border-gray-200 disabled:text-gray-700 disabled:bg-gray-200
+                hover:bg-gtmpink-300 hover:border-gtmpink-300 hover:text-gtmpink-600`,
+  'clear': `bg-transparent font-medium text-dark-aaa
+            disabled:text-dark-a disabled:font-medium hover:font-bold`,
+  'gtm-clear': `bg-transparent border-transparent font-normal text-black
+                disabled:text-gray-600 disabled:font-medium hover:font-bold`,
+};
+
 export type IconPosition = 'left' | 'right';
 
 export interface ButtonProps
@@ -86,13 +116,13 @@ function ButtonRaw({
 }: ButtonProps & ForwardedRefProps<HTMLButtonElement | HTMLAnchorElement>) {
   const buttonClassNames = classNames([
     className,
-    `rounded border-solid border-transparent border gap-x-2.5 mr-4 mb-2 whitespace-nowrap
-      antialiased text-center text-sm no-underline cursor-poinnter focus:outline-0
+    `rounded border-solid border gap-x-2.5 mr-4 mb-2 whitespace-nowrap
+      antialiased text-center text-sm no-underline cursor-pointer focus:outline-0
       disabled:cursor-default disabled:pointer-events-none disabled:opacity-50`,
     size === 'sm' && 'px-3 h-8',
     size === 'md' && 'px-4 h-10',
     size === 'lg' && 'px-10 h-12',
-    cssForVariant(variant),
+    VARIANT_CSS[variant],
     width === '100%' && 'w-full',
     icon && 'flex flex-row justify-center items-center',
   ]);
@@ -141,48 +171,6 @@ function ButtonRaw({
       {content}
     </button>
   );
-}
-
-function cssForVariant(variant) {
-  return classNames([
-    variant === 'primary' &&
-      `bg-dark-brand border-dark-brand text-white font-medium
-      disabled:bg-blue-300 disabled:border-blue-300
-      hover:bg-blue-600 hover:border-blue-600`,
-    variant === 'gtm-primary' &&
-      `bg-black border-black text-white font-normal
-      disabled:bg-gray-400 disabled:border-gray-400 disabled:text-gray-700
-      hover:bg-gtmblue-500 hover:border-gtmblue-500`,
-    variant === 'secondary' &&
-      `bg-white border-blue-500 text-blue-500 font-medium
-      disabled:bg-white disabled:border-blue-300 disabled:text-blue-300
-      hover:bg-slate-100`,
-    variant === 'gtm-secondary' &&
-      `bg-white border-black text-black font-normal
-      disabled:bg-white disabled:border-gray-700 disabled:text-gray-700
-      hover:bg-gtmblue-100 hover:border-gtmblue-500 hover:text-gtmblue-500`,
-    variant === 'gray' &&
-      `bg-gray-100 border-dark-aaa text-dark-aaa font-medium
-      disabled:bg-gray-100 disabled:border-dark-a disabled:text-dark-a
-      hover:bg-slate-200`,
-    variant === 'gtm-gray' &&
-      `bg-gray-200 border-black text-black font-normal hover:bg-gray-400
-      disabled:bg-gray-100 disabled:border-gray-500 disabled:text-gray-500`,
-    variant === 'error' &&
-      `bg-red-100 border-red-100 text-light-error font-medium
-      disabled:border-gray-100 disabled:text-gray-500 disabled:bg-gray-100
-      hover:bg-red-200`,
-    variant === 'gtm-error' &&
-      `bg-gtmpink-100 border-gtmpink-100 text-gtmpink-600 font-formal
-      disabled:border-gray-200 disabled:text-gray-700 disabled:bg-gray-200
-      hover:bg-gtmpink-300 hover:border-gtmpink-300 hover:text-gtmpink-600`,
-    variant === 'clear' &&
-      `bg-transparent font-medium text-dark-aaa
-      disabled:text-dark-a disabled:font-medium hover:font-bold`,
-    variant === 'gtm-clear' &&
-      `bg-transparent font-normal text-black
-      disabled:text-gray-600 disabled:font-medium hover:font-bold`,
-  ]);
 }
 
 const Button = withForwardedRef<ButtonProps, HTMLButtonElement>(ButtonRaw);
