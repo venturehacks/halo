45049c23f22065f9bd11f28f196166045695a4de
Drew Lustro <drew@angel.co>
2022-04-05 15:19:56 -0700

    :package: do not duplicate tailwind config in dist folder

--- packages/halo/rollup.config.js
+++ packages/halo/rollup.config.js
@@ -17,7 +17,6 @@ import copy from 'rollup-plugin-copy';
 import svgr from '@svgr/rollup';
 import tailwindcss from 'tailwindcss';
 
-// @ts-ignore
 import pkg from './package.json';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -111,9 +110,6 @@ export default defineConfig({
       'exclude': ['node_modules/**', '../../node_modules/**'],
       'preventAssignment': true,
       'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    copy({
-      targets: [{ src: './tailwind.config.js', dest: 'dist/' }],
     }),
     // clear out duplicate CSS bundle in CommonJS folder
     del({
