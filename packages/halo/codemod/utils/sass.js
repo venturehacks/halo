97eb080cc3362401163b4514cf1ab4aa762d2afd
Vishal Jeet <vishal@angel.co>
2022-04-18 17:34:31 -0400

    move tempo replacement into a function

--- packages/halo/codemod/utils/sass.js
+++ packages/halo/codemod/utils/sass.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
 const fs = require('fs');
 const postcss = require('postcss');
 const { getTailwindUtils } = require('tailwind-mappings');
@@ -13,14 +15,14 @@ function getTailwindPropertiesForClass(fileName, className) {
 
   const fileConstants = {};
   parsedCss.nodes
-    .filter(node => node.type === 'decl')
-    .forEach(decl => {
+    .filter((node) => node.type === 'decl')
+    .forEach((decl) => {
       fileConstants[decl.prop] = decl.value;
     });
 
   // We have all the parsed css nodes - iterate and fetch the css for matching classname
   const matchingNodes = parsedCss.nodes.filter(
-    node => node.type === 'rule' && node.selector === `.${className}`,
+    (node) => node.type === 'rule' && node.selector === `.${className}`,
   );
 
   if (matchingNodes.length === 0) {
@@ -32,7 +34,7 @@ function getTailwindPropertiesForClass(fileName, className) {
   const cssDeclarations = extractCssDeclarations(classNode);
 
   // Convert each of the declarations into its tailwind value
-  const tailwindClasses = cssDeclarations.map(declaration => {
+  const tailwindClasses = cssDeclarations.map((declaration) => {
     const transformedDecl = {
       prop: declaration.prop,
       value: declaration.value,
@@ -40,16 +42,14 @@ function getTailwindPropertiesForClass(fileName, className) {
 
     try {
       // replace any file constants with actual value
+      // eslint-disable-next-line no-prototype-builtins
       if (fileConstants.hasOwnProperty(transformedDecl.value)) {
         transformedDecl.value = fileConstants[transformedDecl.value];
       }
+
       // replace tempo(x) values with pixel value
-      const groups = transformedDecl.value.match(/tempo\((.*?)\)/);
-      if (groups && groups.length > 1) {
-        const tempoVal = groups[1];
-        const pixelVal = parseFloat(tempoVal) * TEMPO_VAL_PX;
-        transformedDecl.value = `${pixelVal}px`;
-      }
+      transformedDecl.value = replaceTempoWithPixel(transformedDecl.value);
+
       const tailwindVal = getTailwindUtils(transformedDecl);
 
       if (tailwindVal === '') {
@@ -62,7 +62,33 @@ function getTailwindPropertiesForClass(fileName, className) {
     }
   });
 
-  return tailwindClasses.filter(c => c !== '');
+  return tailwindClasses.filter((c) => c !== '');
+}
+
+/**
+ * Returns a string that replaces tempo function calls with the respective pixel value.
+ * e.g. "tempo(1) tempo(2)" => "4px 8px"
+ *
+ * NOTE: this does not work on strings that contain other words as well.
+ * e.g. "tempo(1) tempo(2) 3px" => "4px 8px"
+ */
+function replaceTempoWithPixel(input) {
+  const tempoValRegex = /tempo\((.*?)\)/;
+  const tempoStringRegex = /tempo\((.*?)\)/g;
+
+  const matches = input.match(tempoStringRegex);
+  if (!matches || matches.length === 0) {
+    return input;
+  }
+
+  let result = '';
+  matches.forEach((tempoString) => {
+    const tempoVal = tempoString.match(tempoValRegex)[1];
+    const pixelVal = parseFloat(tempoVal) * TEMPO_VAL_PX;
+    result += `${pixelVal}px `;
+  });
+
+  return result;
 }
 
 function logTransformationError(declaration) {
@@ -80,7 +106,7 @@ function extractCssDeclarations(classNode) {
   // TODO: how do we handle inner classes?
   // TODO: how do we deal with mixins?
 
-  return classNode.nodes.filter(node => node.type === 'decl');
+  return classNode.nodes.filter((node) => node.type === 'decl');
 }
 
 export { getTailwindPropertiesForClass };
