2388ab5264d67ce8bd6badba2dac894691922186
Dominique <dominique@angel.co>
2022-06-13 14:10:41 -0700

    fix size report, update purge files

--- packages/halo/tailwind.config.js
+++ packages/halo/tailwind.config.js
@@ -19,8 +19,7 @@ module.exports = {
   },
   content: [
     path.join(__dirname, 'src/**/*.{tsx,jsx,js,mdx}'),
-    path.join(__dirname, 'dist/**/*.mjs'),
-    path.join(__dirname, 'dist/**/*.mjs'),
+    path.join(__dirname, 'dist/**/*.js'),
     path.join(__dirname, '../docs/src/gatsby-theme-docz/**/*.{tsx,jsx,js,mdx}'),
   ],
   theme: {
